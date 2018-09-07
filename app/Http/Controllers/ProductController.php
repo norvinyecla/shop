395a02867b0e5e6822f5110662b0a9543527c013
Norvin Yecla <ngydev@outlook.com>
2018-09-07 22:25:32 +1000

    Update controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,36 +8,36 @@ class ProductController extends Controller
 {
     public function index() 
     {
-
+        return view('product.index');
     }
 
     public function details(int $id)
     {
-
+        return view('product.details');
     }
 
     public function create()
     {
-
+        return view('product.create');
     }
     
     public function edit(int $id)
     {
-
+        return view('product.edit');
     }
 
     public function add()
     {
-
+        return view('product.create');
     }
 
     public function update(int $id)
     {
-
+        return view('product.edit');
     }
 
     public function delete(int $id)
     {
-
+        return view('product.list');
     }
 }
