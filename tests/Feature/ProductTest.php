7b84dd932ab9a7f565998d77e43ffbed969bd41f
Norvin Yecla <ngydev@outlook.com>
2018-09-07 22:27:37 +1000

    Add initial feature tests

---
+++ tests/Feature/ProductTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProductTest extends TestCase
+{
+    /**
+     * Test product index
+     *
+     * @return void
+     */
+    public function testProductIndex()
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test product create page
+     *
+     * @return void
+     */
+    public function testProductCreate()
+    {
+        $response = $this->get('/create');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test product edit page
+     *
+     * @return void
+     */
+    public function testProductEdit()
+    {
+        $response = $this->get('/edit/123');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test product details page
+     *
+     * @return void
+     */
+    public function testProductDetails()
+    {
+        $response = $this->get('/details/123');
+        $response->assertStatus(200);
+    }
+}
