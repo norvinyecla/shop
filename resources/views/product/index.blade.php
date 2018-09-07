ada040bf17540617d56a832d23d671e423f89372
Norvin Yecla <ngydev@outlook.com>
2018-09-07 22:26:04 +1000

    Add new views for product pages

---
+++ resources/views/product/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>List Products</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <p>List of products page.</p>
+    <a href="/create">Create a new product</a>
+</body>
+</html>
