4749a62d23fac773080998293fecab6f62ff7b0e
Norvin Yecla <ngydev@outlook.com>
2018-09-07 13:03:32 +1000

    Create ProductController

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index() 
+    {
+
+    }
+
+    public function details(int $id)
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+    
+    public function edit(int $id)
+    {
+
+    }
+
+    public function add()
+    {
+
+    }
+
+    public function update(int $id)
+    {
+
+    }
+
+    public function delete(int $id)
+    {
+
+    }
+}
