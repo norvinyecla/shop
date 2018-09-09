6eca823d5f4ffcc6d62b174805a6f73b96f00157
Norvin Yecla <ngydev@outlook.com>
2018-09-09 22:29:22 +1000

    Add tests to check file type

--- tests/Unit/ProductTest.php
+++ tests/Unit/ProductTest.php
@@ -5,6 +5,8 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 class ProductTest extends TestCase
 {
@@ -48,7 +50,139 @@ class ProductTest extends TestCase
         ];
 
         $response = $this->json('POST', '/api/add', $data);
-        var_dump($response->json());
         $response->assertStatus(422);
     }
+
+    /**
+     * Create new product with invalid image (bmp)
+     *
+     * @return void
+     */
+    public function testCreateNewProductWithBMPImage()
+    {
+        Storage::fake('avatars');
+
+        $file = UploadedFile::fake()->image('product.bmp');
+
+        $data = [
+            'name' => 'test name',
+            'price' => 100,
+            'description' => 'test description',
+            'picture' => $file
+        ];
+
+        $response = $this->json('POST', '/api/add', $data);
+        $response->assertStatus(422);
+    }
+
+    /**
+     * Create new product with invalid image (svg)
+     *
+     * @return void
+     */
+    public function testCreateNewProductWithSVGImage()
+    {
+        Storage::fake('avatars');
+
+        $file = UploadedFile::fake()->image('product.svg');
+
+        $data = [
+            'name' => 'test name',
+            'price' => 100,
+            'description' => 'test description',
+            'picture' => $file
+        ];
+
+        $response = $this->json('POST', '/api/add', $data);
+        $response->assertStatus(422);
+    }
+
+    /**
+     * Create new product with valid image (jpeg)
+     *
+     * @return void
+     */
+    public function testCreateNewProductWithJPEGImage()
+    {
+        Storage::fake('uploads');
+
+        $file = UploadedFile::fake()->image('product.jpeg');
+
+        $data = [
+            'name' => 'test name',
+            'price' => 100,
+            'description' => 'test description',
+            'picture' => $file
+        ];
+
+        $response = $this->json('POST', '/api/add', $data);
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Create new product with valid image (jpg)
+     *
+     * @return void
+     */
+    public function testCreateNewProductWithJPGImage()
+    {
+        Storage::fake('uploads');
+
+        $file = UploadedFile::fake()->image('product.jpg');
+
+        $data = [
+            'name' => 'test name',
+            'price' => 100,
+            'description' => 'test description',
+            'picture' => $file
+        ];
+
+        $response = $this->json('POST', '/api/add', $data);
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Create new product with valid image (gif)
+     *
+     * @return void
+     */
+    public function testCreateNewProductWithGIFImage()
+    {
+        Storage::fake('uploads');
+
+        $file = UploadedFile::fake()->image('product.gif');
+
+        $data = [
+            'name' => 'test name',
+            'price' => 100,
+            'description' => 'test description',
+            'picture' => $file
+        ];
+
+        $response = $this->json('POST', '/api/add', $data);
+        $response->assertStatus(200);
+    }
+
+     /**
+     * Create new product with valid image (gif)
+     *
+     * @return void
+     */
+    public function testCreateNewProductWithPNGImage()
+    {
+        Storage::fake('uploads');
+
+        $file = UploadedFile::fake()->image('product.png');
+
+        $data = [
+            'name' => 'test name',
+            'price' => 100,
+            'description' => 'test description',
+            'picture' => $file
+        ];
+
+        $response = $this->json('POST', '/api/add', $data);
+        $response->assertStatus(200);
+    }
+    
 }
