14433c1a115bd6740aadcc81db94aa5281c9215e
Norvin Yecla <ngydev@outlook.com>
2018-09-11 23:32:21 +1000

    Use vue component for adding a product

---
+++ resources/js/components/ProductForm.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class='row'>
+        <form action="#" enctype="multipart/form-data" @submit.prevent="createProduct()">
+            <div class="input-group">
+                <fieldset>
+                    <label>Name</label>
+                    <input v-model="product.name" type="text" name="name" class="form-control" autofocus>
+                </fieldset>
+                <fieldset>
+                    <label>Description</label>
+                    <input v-model="product.description" type="text" name="description" class="form-control">
+                </fieldset>
+                <fieldset>
+                    <label>Price</label>
+                    <input v-model="product.price" type="text" name="price" class="form-control">
+                </fieldset>
+                <fieldset>
+                    <label>Picture</label>
+                    <input v-on:change=onFileChange type="file" ref="picture" name="picture" class="form-control" autofocus>
+                </fieldset>
+                <span class="input-group-btn">
+                    <button type="submit" class="btn btn-primary">{{ message }}</button>
+                </span>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                message: 'Create new product',
+                product: {
+                    name: '',
+                    description: '',
+                    price: 0,
+                    picture: ''
+                }
+            };
+        },
+        
+        created() {
+            
+        },
+        
+        methods: {
+            onFileChange(e) {
+                this.product.picture = this.$refs.picture.files[0];
+            },
+ 
+            createProduct() {
+                var formData = new FormData()
+                formData.append("name", this.product.name); 
+                formData.append("price", this.product.price);
+                formData.append("description", this.product.description);
+                formData.append("picture", this.product.picture);
+                axios(
+                    {
+                        method: 'POST',
+                        url: 'api/add', 
+                        data: formData,
+                        headers: {
+                            'Content-Type': 'multipart/form-data'
+                        }
+                    }
+                ).then((res) => {
+                        this.product.name = '';
+                        this.product.description = '';
+                        this.product.price = 0;
+                        this.product.picture = null;
+                        this.edit = false;
+                        location.href = "/";
+
+                    })
+                    .catch((err) => {
+                        alert(err)
+                        var warning = 'asd'
+                        err.response.data.errors.forEach(function (item, index) {
+                            console.log(item)
+                        })
+                        console.log(warning)
+                        
+                    });
+            }
+        }
+    }
+</script>
--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -1,14 +1,24 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Create Product</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
-</head>
-<body>
-    <p>Create product page.</p>
-</body>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Create New Product</title>
+
+        <!-- CSRF Stuff -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <script>window.Laravel = { csrfToken: '{{ csrf_token() }}' }</script>
+   </head>
+   <body>
+        <a href='/'>Go Back Home</a>
+        <h1>Create New Product</h1>
+        <div class="container" id='app'>
+            <product-form></product-form>
+        </div>
+        
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
 </html>
